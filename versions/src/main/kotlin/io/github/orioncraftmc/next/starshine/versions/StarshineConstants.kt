a41a7760a113a0e498891bf102fdda94270c2783
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-08-20 21:46:53 +0100

    Misc changes

---
+++ versions/src/main/kotlin/io/github/orioncraftmc/next/starshine/versions/StarshineConstants.kt
@@ -0,0 +1,5 @@
+package io.github.orioncraftmc.next.starshine.versions
+
+object StarshineConstants {
+    const val VERSION_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+}
--- versions/src/main/kotlin/io/github/orioncraftmc/next/starshine/versions/StarshineVersions.kt
+++ versions/src/main/kotlin/io/github/orioncraftmc/next/starshine/versions/StarshineVersions.kt
@@ -4,13 +4,20 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonMapperBuilder
 import com.fasterxml.jackson.module.kotlin.kotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.github.orioncraftmc.next.starshine.versions.model.manifest.VersionManifestModel
+import java.net.URL
 
 object StarshineVersions {
 
-    val mapper = jacksonMapperBuilder()
+    internal val mapper = jacksonMapperBuilder()
         .addModule(kotlinModule())
         .addModule(JavaTimeModule())
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .build()
+
+    fun readVersionManifestModel(url: URL = URL(StarshineConstants.VERSION_MANIFEST_URL)): VersionManifestModel {
+        return mapper.readValue(url)
+    }
 
 }
